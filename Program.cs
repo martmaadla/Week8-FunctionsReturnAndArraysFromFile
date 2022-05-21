33b0c5d784f1a525e1a603b7d0d75b5dbb6fea97
Märt Maadla <mart.maadla@reaalprojekt.ee>
2022-05-21 20:42:56 +0300

    week8

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsReturnAndArraysFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, randomMovie;
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            string[] movies = { "Home Alone", "Gee Guy", "Frozen", "Joker", "Forrest Gump", "Alien" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);
+            randomMovie = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Today you will have some {randomFood} with {randomDrink} and watch {randomMovie}");
+            Console.ReadLine();
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random r = new Random();
+            int randomIndex = r.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+        }
+    }
+}
